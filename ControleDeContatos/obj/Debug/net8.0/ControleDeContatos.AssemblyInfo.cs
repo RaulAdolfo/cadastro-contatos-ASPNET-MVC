d99deba392d8f6bc4ebbe363737b022b522b8cc6
RaulAdolfo <raulcesara@gmail.com>
2025-09-18 21:26:25 -0300

    Sistema de paginação JQuery

--- ControleDeContatos/obj/Debug/net8.0/ControleDeContatos.AssemblyInfo.cs
+++ ControleDeContatos/obj/Debug/net8.0/ControleDeContatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7116bd36b3dd86120db95885f28514421438ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332d3940122b3b4c3067654fa525d7bf0e6fff70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
